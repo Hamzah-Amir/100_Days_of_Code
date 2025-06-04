342a614d57fd0758248db76e6c4be0d3b67a1fce
Hamza <hamzaamir7651@gmail.com>
2025-06-04 13:24:59 +0500

    Lambda functions in Python

---
+++ 28_lambda_function/main.py
@@ -0,0 +1,16 @@
+# Our old approach to make simple expression functions like this one
+def cube(x):
+    return x*x*x
+
+# We can use a lambda function to achieve the same result in a more concise way
+cube = lambda x: x*x*x
+print(cube(3))
+
+# We can also use lambda functions to create more complex expressions
+def add(x, y):
+    return x + y
+
+add = lambda x, y: x + y
+print(add(3, 4))
+
+# Note only use lamba functions for simple expressions
