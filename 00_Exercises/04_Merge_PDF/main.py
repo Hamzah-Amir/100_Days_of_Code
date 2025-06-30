6453f5e652110c3f191144fbd4f927fcffd9b2cd
Hamza <hamzaamir7651@gmail.com>
2025-06-30 13:32:46 +0500

    Exercise Merge the PDFs

--- 00_Exercises/04_Merge_PDF/main.py
+++ 00_Exercises/04_Merge_PDF/main.py
@@ -3,11 +3,12 @@ from PyPDF2 import PdfWriter
 
 merge = PdfWriter()
 path = r'D:\100_Days_of_Code\00_Exercises\04_Merge_PDF\pdfs'
-for pdf in os.listdir(path):
+files = os.listdir(path)
+for pdf in files:
     if pdf.endswith(".pdf"):
         merge.append(os.path.join(path, pdf))
 
 new_pdf = merge.write("Merged-PDF.pdf")
 merge.close()
 
-os.rename("Merged-PDF.pdf",f"{path}/Merged-PDF-2023.pdf")
+os.rename("Merged-PDF.pdf",f"{path}/Merged-PDF-2025.pdf")
